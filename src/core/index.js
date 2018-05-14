04e45cbb021c61ce09611ec57dc118d7c2c5b7d9
dongtongtong <dongtongtong@meituan.com>
2018-05-14 21:27:57 +0800

    init

---
+++ src/core/index.js
@@ -0,0 +1,8 @@
+import 'es6-promise/auto';
+import 'normalize.css';
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'core/styles/index.scss';
+
+Vue.use(ElementUI);
