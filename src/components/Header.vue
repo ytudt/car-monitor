69f31d93c0f7881b851b8fddebcbea04ed501918
dongtongtong <dongtongtong@meituan.com>
2018-05-15 19:39:11 +0800

    feat:登录页面

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .header-wrap
+    slot
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '~core/styles/index';
+  .header-wrap{
+    padding: 0 20px;
+    width: 100%;
+    height: 60px;
+    background: $base-color;
+  }
+</style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,48 @@
 <template lang="pug">
-  h1 我是登录页面
+.login-wrap
+  Header
+  .form-wrap
+    el-form
+      el-form-item(label="用户名")
+        el-input(v-model="userName")
+      el-form-item(label="密码")
+        el-input(:type="passwordType" v-model="password")
+        span.show-password(@click="showPassword=!showPassword") {{showPassword ? 'HIDE' : 'SHOW'}}
+    el-button(type="primary" :disabled="!userName&&!password") 登录
 </template>
 
 <script>
+  import MD5 from 'md5';
+  import Header from 'components/Header';
   export default {
-    name: 'HelloWorld',
+    name: 'Login',
+    components: {
+      Header
+    },
     data () {
       return {
+        showPassword: false,
+        userName: '',
+        password: '',
       }
-    }
+    },
+    computed:{
+      passwordType(){
+        return this.showPassword ? '' : 'password';
+      },
+    },
+    created(){
+      console.log(MD5('123'));
+    },
   }
 </script>
 <style scoped lang="scss">
+  .form-wrap{
+    width: 400px;
+    .show-password{
+      position: absolute;
+      right: 10px;
+      color: #666;
+    }
+  }
 </style>
